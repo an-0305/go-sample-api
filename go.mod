4c38f544b86561986b4067374f460746564b53fd
akari nakamura <akarinakamura0305@gmail.com>
2021-06-14 22:04:53 +0900

    dbに接続

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module go-sample-api
 
 go 1.16
 
-require entgo.io/ent v0.8.0 // indirect
+require (
+	entgo.io/ent v0.8.0 // indirect
+	github.com/jackc/pgx/v4 v4.11.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,8 +1,16 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"fmt"
+	"go-sample-api/ent"
+	"log"
 	"net/http"
+
+	"entgo.io/ent/dialect"
+	entsql "entgo.io/ent/dialect/sql"
+	_ "github.com/jackc/pgx/v4/stdlib"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -10,7 +18,29 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 }
 
+func Open(databaseUrl string) *ent.Client {
+	db, err := sql.Open("pgx", databaseUrl)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	drv := entsql.OpenDB(dialect.Postgres, db)
+	return ent.NewClient(ent.Driver(drv))
+}
+
 func main() {
+	client := Open("postgresql://postgres:test@127.0.0.1")
+
+	ctx := context.Background()
+	if err := client.Schema.Create(ctx); err != nil {
+		log.Fatal(err)
+	}
+	users, err := client.User.Query().All(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(users)
+
 	http.HandleFunc("/", handler)
 	http.ListenAndServe(":8080", nil)
 }
